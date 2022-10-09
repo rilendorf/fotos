9d407685451d2737a42b9a6a5dbb5a3a27c89f92
riley <riley@e926.de>
2022-10-09 12:47:00 +0200

    some checks to prevent taking multible pictures at the same time

--- libcamera/camera.go
+++ libcamera/camera.go
@@ -4,6 +4,9 @@ import (
 	"github.com/DerZombiiie/go-libcamera-example/collector"
 
 	"bytes"
+	"errors"
+
+	"log"
 	"os"
 	"os/exec"
 	"syscall"
@@ -12,8 +15,18 @@ import (
 	"github.com/DerZombiiie/fotos/fotos"
 )
 
+type Status uint8
+
+const (
+	Idle Status = iota
+	Ready
+	WaitingFrame
+	Sleep
+)
+
 type Webcam struct {
 	process *exec.Cmd
+	status  Status
 
 	frames chan []byte
 
@@ -26,13 +39,35 @@ func (w *Webcam) ReadNextFrame() []byte {
 }
 
 func (w *Webcam) Ready() {
+	if w.status == Idle {
+		w.status = Ready
+	} else {
+		log.Println("Ready pressed too often in a short timespan, ignoring")
+	}
 }
 
+var (
+	ErrInvalidStatus = errors.New("Invalid Status while taking Picture")
+)
+
 func (w *Webcam) TakePicture() (*fotos.Image, error) {
+	if w.status != Ready {
+		log.Printf("Trying to take picture with status != Ready, status: %s \n", w.status.String())
+		return nil, ErrInvalidStatus
+	}
+
+	w.status = WaitingFrame
+
 	w.process.Process.Signal(syscall.SIGUSR1)
 
 	// read image:
 	bytes := w.ReadNextFrame()
+	w.status = Sleep
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+
+		w.status = Idle
+	}()
 
 	return fotos.ImageFromBytes(bytes), nil
 }
