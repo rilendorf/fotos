33acb5c3e7aaaa94a97f2f9762a0e22e3c85e4d9
riley <riley@e926.de>
2022-10-10 20:54:28 +0200

    cmd/fotos: some more testing

--- cmd/fotos/main.go
+++ cmd/fotos/main.go
@@ -8,8 +8,12 @@ import (
 	_ "github.com/DerZombiiie/fotos/input_gpio"
 	_ "github.com/DerZombiiie/fotos/local_webinterface"
 	_ "github.com/DerZombiiie/fotos/ui_console"
-	_ "github.com/DerZombiiie/fotos/ui_webview"
-	_ "github.com/DerZombiiie/fotos/webcam"
+	//	_ "github.com/DerZombiiie/fotos/ui_webview"
+	//_ "github.com/DerZombiiie/fotos/libcamera_singleshot"
+	_ "github.com/DerZombiiie/fotos/libcamera"
+	// _ "github.com/DerZombiiie/fotos/ui_glfw"
+	_ "github.com/DerZombiiie/fotos/ui_neopixel"
+	// _ "github.com/DerZombiiie/fotos/ui_webview"
 )
 
 func main() {
