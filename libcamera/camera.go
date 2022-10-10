dce000330480ab5a69f14804159af4f14e8425c1
riley <riley@e926.de>
2022-10-10 19:18:26 +0200

    libcamera: moved back to tmpfile cuz of crashes
    Now using a temporary file all images are written to.

--- libcamera/camera.go
+++ libcamera/camera.go
@@ -1,94 +1,141 @@
 package webcam
 
 import (
-	"github.com/DerZombiiie/go-libcamera-example/collector"
-
-	"bytes"
-	"errors"
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/mitchellh/go-ps"
+	"path"
 
 	"log"
 	"os"
-	"os/exec"
 	"syscall"
 	"time"
 
 	"github.com/DerZombiiie/fotos/fotos"
 )
 
-type Status uint8
-
-const (
-	Idle Status = iota
-	Ready
-	WaitingFrame
-	Sleep
-)
-
-type Webcam struct {
-	process *exec.Cmd
-	status  Status
-
-	frames chan []byte
-
-	tmpDir  string
-	tmpFile string
+type Camera struct {
+	takeImage chan struct{}
+	recImage  chan []byte
 }
 
-func (w *Webcam) ReadNextFrame() []byte {
-	return <-w.frames
-}
+func (c *Camera) CameraThread() {
+	c.takeImage = make(chan struct{})
+	c.recImage = make(chan []byte)
 
-func (w *Webcam) Ready() {
-	if w.status == Idle {
-		w.status = Ready
-	} else {
-		log.Println("Ready pressed too often in a short timespan, ignoring")
+	l, err := ps.Processes()
+	if err != nil {
+		fmt.Printf("Error: %s \n", err)
+		return
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Printf("Error: %s \n", err)
+		return
+	}
+
+	watcher.Add("image.jpg")
+
+	for _, p := range l {
+		_, f := path.Split(p.Executable())
+
+		pid := p.Pid()
+
+		if f == "libcamera-still" {
+			fmt.Printf("Found libcamera-still process (PID: %d) \n", pid)
+
+			if err != nil {
+				fmt.Printf("Error Finding Process (PID: %d): %s \n", pid, err)
+				return
+			}
+
+			for {
+				fmt.Println("waiting for channel takeImage")
+				<-c.takeImage
+
+				fmt.Printf("sending signal to PID %d \n", pid)
+
+				pr, err := os.FindProcess(pid)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				pr.Signal(syscall.SIGUSR1)
+
+			loop:
+				for {
+					log.Println("Waiting for events")
+					c := time.NewTimer(time.Second * 3).C
+
+					select {
+					case <-c:
+						log.Println("Timeout waiting for events!")
+						break loop
+
+					case e := <-watcher.Events:
+
+						fmt.Printf("Event: %s, op: %s \n", e, e.Op)
+
+						switch e.Op {
+						case fsnotify.Remove:
+						case fsnotify.Rename:
+						case fsnotify.Chmod:
+							continue
+
+						case fsnotify.Write:
+							fallthrough
+						case fsnotify.Create:
+							time.Sleep(time.Second * 2)
+
+							break loop
+						}
+					case e := <-watcher.Errors:
+						log.Printf("watcher: error: %s \n", e)
+					}
+				}
+
+				file := "tmpimage.jpg"
+
+				log.Println("Renaming...")
+				err = os.Rename("image.jpg", file)
+				if err != nil {
+					log.Printf("Error renaming: %s \n", err)
+				}
+				log.Println("Done.")
+
+				b, err := os.ReadFile(file)
+				if err != nil {
+					c.recImage <- []byte{}
+					fmt.Printf("error reading file %s: %s \n", file, err)
+				}
+
+				c.recImage <- b
+
+				// timeout
+				time.Sleep(time.Second * 2)
+				log.Println("done taking image, returning to:")
+			}
+		}
 	}
 }
 
-var (
-	ErrInvalidStatus = errors.New("Invalid Status while taking Picture")
-)
-
-func (w *Webcam) TakePicture() (*fotos.Image, error) {
-	if w.status != Ready {
-		log.Printf("Trying to take picture with status != Ready, status: %s \n", w.status.String())
-		return nil, ErrInvalidStatus
-	}
-
-	w.status = WaitingFrame
-
-	w.process.Process.Signal(syscall.SIGUSR1)
+func (c *Camera) Ready(time.Duration) {}
+func (c *Camera) TakePicture() (*fotos.Image, error) {
+	c.takeImage <- struct{}{}
 
 	// read image:
-	bytes := w.ReadNextFrame()
-	w.status = Sleep
-	go func() {
-		time.Sleep(time.Millisecond * 100)
-
-		w.status = Idle
-	}()
+	bytes := <-c.recImage
 
 	return fotos.ImageFromBytes(bytes), nil
 }
 
 func init() {
 	fotos.Runner(func() {
-		cmd := exec.Command("libcamera-still", "-t0", "-s", "-o-", "-ejpg", "--vflip", "-f")
+		c := &Camera{}
 
-		ch := make(chan []byte)
+		go c.CameraThread()
 
-		cmd.Stdout = collector.MakeCollector(time.Millisecond*250, func(buf *bytes.Buffer) {
-			ch <- buf.Bytes()
-		})
-
-		cmd.Stderr = os.Stderr
-
-		cmd.Start()
-
-		fotos.RegisterCam("libcamera", &Webcam{
-			process: cmd,
-			frames:  ch,
-		})
+		fotos.RegisterCam("libcamera", c)
 	})
 }
